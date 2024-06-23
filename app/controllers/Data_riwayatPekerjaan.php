b1facdb9d77a0a4dc43d45f5d7236f9326762159
SyaefulBachtiar <syaefulbachtiar2@gmail.com>
2024-06-23 15:53:47 +0700

    ubah

--- app/controllers/Data_riwayatPekerjaan.php
+++ app/controllers/Data_riwayatPekerjaan.php
@@ -12,14 +12,15 @@ class Data_riwayatPekerjaan extends Controller{
 
 
 
-    public function read_saved_rp(){
+    public function read_saved_rp($nip){
+            $np = $this->model('Calon_Pensiunan')->getIdRelation($nip);
         if( $this->model('Calon_Pensiunan')->input_riwayatPekerjaan($_POST) > 0){
             Flasher::setFlash('berhasil!', 'ditambahkan', 'success');
-            header('Location:'. BASEURL . '/Dashboard_calonpensiun');
+            header('Location:'. BASEURL . '/Dashboard_calonpensiun/index'. $np);
             exit;
         }else{
             Flasher::setFlash('gagal!', 'ditambahkan', 'danger');
-            header('Location:'. BASEURL . '/Dashboard_calonpensiun');
+            header('Location:'. BASEURL . '/Dashboard_calonpensiun/index'. $np);
             exit;
         }
     }
